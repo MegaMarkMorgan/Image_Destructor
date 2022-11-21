eea11e995efdbabe3635e04a045da92f4a42ec0c
MegaMarkMorgan <108385984+MegaMarkMorgan@users.noreply.github.com>
2022-11-21 15:39:18 +0800

    Add files via upload

--- Image_Destructor.py
+++ Image_Destructor.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, url_for, redirect, render_template
 from PIL import Image, ImageFilter
-import urllib.request
 import random
-import gunicorn
 
 
 app = Flask(__name__)
@@ -11,26 +9,24 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pic = Image.open("static/digital_memory.png")
+        pic = Image.open("/home/MarkMorgan/mysite/static/digital_memory.png")
         def func1():
-            pic.rotate(90).save("static/digital_memory.png")
+            pic.rotate(45).save("/home/MarkMorgan/mysite/static/digital_memory.png")
         def func2():
-            pic.quantize(colors=124).save("static/digital_memory.png")
+            pic.quantize(colors=124).save("/home/MarkMorgan/mysite/static/digital_memory.png")
         def func3():
-            pic.resize((1002, 1600)).save("static/digital_memory.png")
+            pic.resize((1002, 1600)).save("/home/MarkMorgan/mysite/static/digital_memory.png")
         def func4():
-            pic.convert('RGB').reduce(2).save("static/digital_memory.png")
+            pic.convert('RGB').reduce(2).save("/home/MarkMorgan/mysite/static/digital_memory.png")
         def func5():
-            pic.convert('RGB').filter(filter=ImageFilter.SHARPEN).save("static/digital_memory.png")
+            pic.convert('RGB').filter(filter=ImageFilter.SHARPEN).save("/home/MarkMorgan/mysite/static/digital_memory.png")
         def func6():
             HSV= pic.convert('HSV')
             H, S, V = HSV.split()
             H = H.point(lambda p: p+40)
             HSVr = Image.merge('HSV', (H,S,V))
             RGBr = HSVr.convert('RGB')
-
-# Save processed result
-            RGBr.save("static/digital_memory.png")
+            RGBr.save("/home/MarkMorgan/mysite/static/digital_memory.png")
         my_list = [func1, func2, func3, func4, func5, func6,]
         random.choice(my_list)()
 
